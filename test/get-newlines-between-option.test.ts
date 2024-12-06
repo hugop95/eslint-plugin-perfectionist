665c7df104513cda3a27046fc43d1c385979ff20
hugo.prunaux <hugo.prunaux@gmail.com>
2024-12-06 02:19:44 +0100

    todo commit split

---
+++ test/get-newlines-between-option.test.ts
@@ -0,0 +1,79 @@
+import { describe, expect, it } from 'vitest'
+
+import type { SortingNode } from '../typings'
+
+import { getNewlinesBetweenOption } from '../utils/get-newlines-between-option'
+
+describe('get-newlines-between-option', () => {
+  describe('global "newlinesBetween" option', () => {
+    it('should return the global "newlinesBetween" option if "customGroups" is not an array', () => {
+      expect(
+        getNewlinesBetweenOption({
+          options: {
+            newlinesBetween: 'ignore',
+            customGroups: {},
+            groups: [],
+          },
+          nextSortingNode: generateSortingNodeWithGroup('unknown'),
+          sortingNode: generateSortingNodeWithGroup('unknown'),
+        }),
+      ).toBe('ignore')
+    })
+
+    it('should return "ignore" if "newlinesBetween" is "ignore"', () => {
+      expect(
+        getNewlinesBetweenOption({
+          options: {
+            newlinesBetween: 'ignore',
+            groups: [],
+          },
+          nextSortingNode: generateSortingNodeWithGroup('unknown'),
+          sortingNode: generateSortingNodeWithGroup('unknown'),
+        }),
+      ).toBe('ignore')
+    })
+
+    it('should return "never" if "newlinesBetween" is "never"', () => {
+      expect(
+        getNewlinesBetweenOption({
+          options: {
+            newlinesBetween: 'never',
+            groups: [],
+          },
+          nextSortingNode: generateSortingNodeWithGroup('unknown'),
+          sortingNode: generateSortingNodeWithGroup('unknown'),
+        }),
+      ).toBe('never')
+    })
+
+    it('should return "global_always" if "newlinesBetween" is "always" and nodeGroupNumber !== nextNodeGroupNumber', () => {
+      expect(
+        getNewlinesBetweenOption({
+          options: {
+            newlinesBetween: 'always',
+            groups: ['a', 'b'],
+          },
+          nextSortingNode: generateSortingNodeWithGroup('a'),
+          sortingNode: generateSortingNodeWithGroup('b'),
+        }),
+      ).toBe('global_always')
+    })
+
+    it('should return "never" if "newlinesBetween" is "always" and nodeGroupNumber === nextNodeGroupNumber', () => {
+      expect(
+        getNewlinesBetweenOption({
+          options: {
+            newlinesBetween: 'always',
+            groups: [],
+          },
+          nextSortingNode: generateSortingNodeWithGroup('unknown'),
+          sortingNode: generateSortingNodeWithGroup('unknown'),
+        }),
+      ).toBe('never')
+    })
+  })
+
+  let generateSortingNodeWithGroup = (group: string): SortingNode => ({
+      group,
+    } as SortingNode)
+})
