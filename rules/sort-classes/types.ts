0f18fc5f08ed49c9b7f44cb99d706f08bcafda4d
hugo.prunaux <hugo.prunaux@gmail.com>
2025-12-31 13:29:51 +0100

    refactor(sort-classes): use `GroupMatcher`

--- rules/sort-classes/types.ts
+++ rules/sort-classes/types.ts
@@ -105,15 +105,15 @@ type AdditionalSortOptions = object
  * JSON schema generation.
  */
 export let allSelectors = [
+  'function-property',
   'accessor-property',
-  'index-signature',
+  'property',
   'constructor',
-  'static-block',
   'get-method',
   'set-method',
-  'function-property',
-  'property',
   'method',
+  'index-signature',
+  'static-block',
 ] as const
 
 /**
@@ -121,17 +121,17 @@ export let allSelectors = [
  * JSON schema generation.
  */
 export let allModifiers = [
-  'async',
+  'static',
+  'declare',
+  'abstract',
+  'decorated',
+  'override',
+  'readonly',
   'protected',
   'private',
   'public',
-  'static',
-  'abstract',
-  'override',
-  'readonly',
-  'decorated',
-  'declare',
   'optional',
+  'async',
 ] as const
 
 /**
